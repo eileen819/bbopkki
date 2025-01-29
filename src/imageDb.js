2d443409fd7d9cd2918d4686674414596b58b91c
eileen819 <eileen.ju.8819@gmail.com>
2025-01-30 03:02:59 +0900

    이미지 주소 수정

--- src/imageDb.js
+++ src/imageDb.js
@@ -1,62 +1,62 @@
 export const data = [
   {
     id: 1,
-    image: "/image/1.JPG",
+    image: "/public/image/1.JPG",
   },
   {
     id: 2,
-    image: "/image/2.JPG",
+    image: "/public/image/2.JPG",
   },
   {
     id: 3,
-    image: "/image/3.JPG",
+    image: "/public/image/3.JPG",
   },
   {
     id: 4,
-    image: "/image/4.JPG",
+    image: "/public/image/4.JPG",
   },
   {
     id: 5,
-    image: "/image/5.JPG",
+    image: "/public/image/5.JPG",
   },
   {
     id: 6,
-    image: "/image/6.JPG",
+    image: "/public/image/6.JPG",
   },
   {
     id: 7,
-    image: "/image/7.JPG",
+    image: "/public/image/7.JPG",
   },
   {
     id: 8,
-    image: "/image/8.JPG",
+    image: "/public/image/8.JPG",
   },
   {
     id: 9,
-    image: "/image/9.JPG",
+    image: "/public/image/9.JPG",
   },
   {
     id: 10,
-    image: "/image/10.JPG",
+    image: "/public/image/10.JPG",
   },
   {
     id: 11,
-    image: "/image/11.JPG",
+    image: "/public/image/11.JPG",
   },
   {
     id: 12,
-    image: "/image/12.JPG",
+    image: "/public/image/12.JPG",
   },
   {
     id: 13,
-    image: "/image/13.JPG",
+    image: "/public/image/13.JPG",
   },
   {
     id: 14,
-    image: "/image/14.JPG",
+    image: "/public/image/14.JPG",
   },
   {
     id: 15,
-    image: "/image/15.JPG",
+    image: "/public/image/15.JPG",
   },
 ];
